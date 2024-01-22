8d556510c00b39b0782fca1633c6d536fe8b08db
Benjamin Clark <benisin@gmail.com>
2024-01-22 13:34:34 -0800

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,13 +3,13 @@ import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
-  return ( 
-    <div className="h-full ">
+  return (
+    <div className="h-full">
       <LandingNavbar />
       <LandingHero />
       <LandingContent />
     </div>
-   );
-}
- 
+  );
+};
+
 export default LandingPage;
